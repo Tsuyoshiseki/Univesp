1a8c7ae4b0afef2d573b8294f2adcc9073114eba
Luã Seki <158064034+Tsuyoshiseki@users.noreply.github.com>
2026-05-02 14:39:39 -0300

    supabse

--- PIUnivesp - Supabse/Backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-module.exports = db;
